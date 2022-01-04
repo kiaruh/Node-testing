d54c9dd903e92fd5cf570708adee36de15d7f771
kiaruh <nikoz.li@gmail.com>
2022-01-04 01:25:53 -0300

    un poco de fs

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 // var nombre = process.argv[2]
 // console.log('Hola ' + nombre)
 
+
+//usando fs
+// var fs = require('fs')
+// var result = fs.readFileSync('nico.txt', 'utf8') //SINCRONO
+// console.log(result)
+
+// fs.readFile('nico1.txt', (err,data)=> {
+//     if(err) throw err 
+//     console.log(data.toString())
+// }) //ASINCRONO
+
