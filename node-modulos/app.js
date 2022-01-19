d8a5b83f0c4a9cb97e29e8268f4a859cd2c4faef
kiaruh <nikoz.li@gmail.com>
2022-01-19 11:08:01 -0300

    http

--- node-modulos/app.js
+++ node-modulos/app.js
@@ -5,6 +5,7 @@
 // URL para trabajar con las urls
 // PATH para trabajar con los directorios
 
+//EJEMPLOS
 //objetos globales console, setTimeout, setInterval, clearTimeout, clearInterval
 // require('./modulos/datos')
 // console.log(global.test)
@@ -13,16 +14,19 @@
 // dato.log('hola mundo')
 // dato.log(dato.url)
 
+//PATH
 // const path = require('path')
 // const oPath = path.parse(__filename)
 // console.log(oPath)
 
+//OS
 // const os = require('os')
 // var memoriaLibre = os.freemem()
 // var memoriaTotal = os.totalmem()
 // console.log('memoria libre: '+memoriaLibre)
 // console.log('memoria total: '+memoriaTotal)
 
+//FS
 // const fs = require('fs')
 // const archivos = fs.readdirSync('./') //syncrono
 // console.log(archivos)
@@ -35,7 +39,25 @@
 //     }
 // }) //asincrono
 
-const EventEmitter = require('events')
-const emitter = new EventEmitter()
-emitter.on('hola', (arg) => console.log('Escuchando :' + arg + ' ' + arg.id+ ' ' +arg.url ))
-emitter.emit('hola', {id:1, url:'http://www.google.com'})
+//Eventos
+// const EventEmitter = require('events')
+// const emitter = new EventEmitter()
+// emitter.on('hola', (arg) => console.log('Escuchando :' + arg + ' ' + arg.id+ ' ' +arg.url ))
+// emitter.emit('hola', {id:1, url:'http://www.google.com'})
+
+//HTTP
+const http = require('http')
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.write('Hola mundo')
+        res.end()
+    }
+
+    if(req.url === '/api/productos'){
+        res.write(JSON.stringify([{id:1, nombre:'producto 1'}, {id:2, nombre:'producto 2'}]))
+        res.end()
+    }
+
+})
+// server.on('connection', (port) => console.log('Nueva conexion: '+ port))
+server.listen(3000) //escucha en el puerto 3000
