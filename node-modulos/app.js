d943537a62e0f9c3d5a0baee0dacb2550207c01f
kiaruh <nikoz.li@gmail.com>
2022-01-19 10:20:10 -0300

    modulos de node

---
+++ node-modulos/app.js
@@ -0,0 +1,37 @@
+// SO trabajar con el sistema operativa
+// FS para trabajar con el sistema de archivos
+// EVENTS para trabajar con eventos
+// HTTP para trabajar con el protocolo http
+// URL para trabajar con las urls
+// PATH para trabajar con los directorios
+
+//objetos globales console, setTimeout, setInterval, clearTimeout, clearInterval
+// require('./modulos/datos')
+// console.log(global.test)
+
+// const dato = require('./modulos/datos')
+// dato.log('hola mundo')
+// dato.log(dato.url)
+
+// const path = require('path')
+// const oPath = path.parse(__filename)
+// console.log(oPath)
+
+// const os = require('os')
+// var memoriaLibre = os.freemem()
+// var memoriaTotal = os.totalmem()
+// console.log('memoria libre: '+memoriaLibre)
+// console.log('memoria total: '+memoriaTotal)
+
+// const fs = require('fs')
+// const archivos = fs.readdirSync('./') //syncrono
+// console.log(archivos)
+
+// fs.readdir('./', function(err, archivos) {
+//     if (err) {
+//         console.log('Error: '+err)
+//     } else {
+//         console.log(archivos)
+//     }
+// }) //asincrono
+
---
+++ node-modulos/modulos/datos.js
@@ -0,0 +1,15 @@
+// global.test = 'hola mundo'
+
+// var url = 'http://www.google.com'
+
+// function dato(mensaje) {
+//     //envio http request
+//     console.log(mensaje)
+// }
+
+// module.exports.log = dato
+// module.exports.url = url
+
+// console.log(__dirname)
+// console.log(__filename)
+
