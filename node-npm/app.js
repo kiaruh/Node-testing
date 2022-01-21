1b74b9c31e689f6ceb4c67dcfaf5116206b828a8
kiaruh <nikoz.li@gmail.com>
2022-01-20 23:22:11 -0300

    comandos

--- node-npm/app.js
+++ node-npm/app.js
@@ -1,3 +1,15 @@
 const colors = require('colors')
 
 console.log('Iniciando el programa'.green)
+//npm list
+//npm view
+//npm install
+//npm uninstall
+//npm update
+//npm search
+//npm help
+//npm init
+//npm config list
+//npm config get
+//npm config set
+//npm config delete
